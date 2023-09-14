3c390888dc01c61a53a04cd15007cb8dd4b4434d
Andrzej Jedrzejczak <jedrzejczak.andrzej89@gmail.com>
2023-09-14 12:50:59 +0200

    fixup! fix: get reflect from deno_reflect

--- src/deps.ts
+++ src/deps.ts
@@ -13,4 +13,4 @@ export { Reflect } from "https://deno.land/x/deno_reflect@v0.2.1/mod.ts";
 export {
   bootstrap,
   Injectable,
-} from "https://deno.land/x/inject@v0.1.2/mod.ts";
+} from "https://raw.githubusercontent.com/fan17/deno-dependency-injector/master/mod.ts";
